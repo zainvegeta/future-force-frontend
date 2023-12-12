a6859727eda14ebaf78db838a4c8db7880185e17
vishnu <vishnupg@live.com>
2023-12-12 22:40:18 +0530

    12-12-2023:22:40

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,45 +2,65 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
 import { CommonModule } from '@angular/common';
 import { SanitizeHtmlPipe } from '../helpers';
-
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import qs from 'qs';
+declare var $: any;
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,SanitizeHtmlPipe],
+  imports: [CommonModule, SanitizeHtmlPipe, FormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent implements OnInit {
-  categories: any=[];
-  featuredCategories: any=[];
-  countries: any=[];
-  jobs: any=[];
-  jobPage:number=1;
-  jobPageSize=5;
-  featuredJobs: any=[];
-  constructor(private apiService: ApiService) {}
+  categories: any = [];
+  featuredCategories: any = [];
+  countries: any = [];
+  jobs: any = [];
+  jobPage: number = 1;
+  jobPageSize = 5;
+  featuredJobs: any = [];
+
+  constructor(private apiService: ApiService, private router: Router) {}
   ngOnInit(): void {
     this.apiService.getCategories().subscribe((response) => {
       this.categories = response.data;
     });
     this.apiService.getFeaturedCategories().subscribe((response) => {
-      this.featuredCategories = response.data;     
+      this.featuredCategories = response.data;
     });
     this.apiService.getCountries().subscribe((response) => {
       this.countries = response.data;
     });
-    this.apiService.getJobs(this.jobPage,this.jobPageSize).subscribe((response) => {
-      this.jobs = response.data;
-    });
+    this.apiService
+      .getJobs(this.jobPage, this.jobPageSize)
+      .subscribe((response) => {
+        this.jobs = response.data;
+      });
     this.apiService.getFeaturedJobs().subscribe((response) => {
-      this.featuredJobs = response.data;     
+      this.featuredJobs = response.data;
     });
   }
 
-  loadMoreJobs():void{
-    this.jobPage=this.jobPage+1;
-    this.apiService.getJobs(this.jobPage,this.jobPageSize).subscribe((response) => {
-      this.jobs = [ ...this.jobs, ...response.data];
+  loadMoreJobs(): void {
+    this.jobPage = this.jobPage + 1;
+    this.apiService
+      .getJobs(this.jobPage, this.jobPageSize)
+      .subscribe((response) => {
+        this.jobs = [...this.jobs, ...response.data];
+      });
+  }
+
+  onSearchSubmit() {
+    var query = $('#query').val();
+    var countryId = $('#country').val() as number;
+    var categoryId = $('#category').val() as number;
+    let queryString = qs.stringify({
+      query: query,
+      countryId: countryId,
+      categoryId: categoryId,
     });
+    window.location.href=`/jobs?${queryString}`;
   }
 }
--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -2,19 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 declare var $: any;
 @Component({
   selector: 'app-jobs',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './jobs.component.html',
-  styleUrl: './jobs.component.css'
+  styleUrl: './jobs.component.css',
 })
 export class JobsComponent implements OnInit {
-  categories: any=[];
-  countries: any=[];
-  jobs: any=[];
-  constructor(private apiService: ApiService) {}
+  categories: any = [];
+  countries: any = [];
+  jobs: any = [];
+  constructor(private apiService: ApiService, private route: ActivatedRoute) {}
   ngOnInit(): void {
     this.apiService.getCategories().subscribe((response) => {
       this.categories = response.data;
@@ -22,20 +23,29 @@ export class JobsComponent implements OnInit {
     this.apiService.getCountries().subscribe((response) => {
       this.countries = response.data;
     });
-    this.apiService.getJobs().subscribe((response) => {
-      this.jobs = response.data;
+    this.route.queryParams.subscribe((params) => {
+      let query = params['query'];
+      let categoryId = params['categoryId'];
+      let countryId = params['countryId'];
+      if (query && query != '') $('#query').val(query);
+      if (categoryId && categoryId != 0) $('#category').val(categoryId).change();
+      if (countryId && countryId != 0) $('#country').val(countryId).change();
+      this.apiService
+        .getJobs(1, 25, countryId ?? 0, categoryId ?? 0, query ?? '')
+        .subscribe((response) => {
+          this.jobs = response.data;
+        });
     });
   }
 
   onSearchSubmit() {
-    var query=$('#query').val();
-    var countryId=$('#country').val() as number;
-    var categoryId=$('#category').val() as number;
-    console.log('country',countryId);
-    console.log('category',categoryId);
-    this.apiService.getJobs(1,25,countryId,categoryId,query).subscribe((response) => {
-      console.log(response.data);
-      this.jobs = response.data;
-    });
- }
+    var query = $('#query').val();
+    var countryId = $('#country').val() as number;
+    var categoryId = $('#category').val() as number;
+    this.apiService
+      .getJobs(1, 25, countryId, categoryId, query)
+      .subscribe((response) => {
+        this.jobs = response.data;
+      });
+  }
 }
