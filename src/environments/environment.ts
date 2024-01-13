cabc306342aab310d24f45478e7fe5c45accb91c
vishnu <vishnupg@live.com>
2024-01-13 13:43:34 +0530

    13-01-2024:13:40

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://13-232-158-223.nip.io',
-    apiToken:'60c797ba33e83fb92426bc8e916fb78bbf26431052eba5ef31c7c817b540434fc6130bab2dfd1c7910026a0795a2f5cfabc525fdd351436cd88c48de653de06a28a8e5268e1bf3f4e1c018c5c1238a5b2f29f69da0310c6b11a4b7fd80f5ce16ce0c1bb1a6dcf6edfd5a7e358dfc26477009a32a24c76b35fa4224b3c565d846',
+    apiUrl: 'https://admin.futureforceconsultants.com',
+    apiToken:'6e86f00dc37ec579e28c9846099c8d5895dbdc962321098319f0bf93fe6695213b96a8b1f9bd99c6e67583c137cc24d5a7308fe6e630db4d519b908d907467bc23cdd26487f774510c52f3d8e2c8823df60137f088276ccf5f74fd8bad878dae965590cc16ec2f1dbd468fc9fec55ded560b99f08237012e6a872d6add608d94',
     recaptcha: {
         siteKey: '6LdOqk0pAAAAAHJHTnJUH6ZdeXkZU19rVuTLF84a',
       },
