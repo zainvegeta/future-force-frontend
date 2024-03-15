4ad26cd1c49e1ba70780e2c18e3928f7a0d368ec
Hank <38037830+zainvegeta@users.noreply.github.com>
2024-03-15 21:30:13 +0530

    changed name of george to roby

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,16 @@
-export const environment = {
+/* export const environment = {
     production: false,
     apiUrl: 'http://localhost:1337',
     apiToken:'b401afb8d9ee2bb307d9e7e2feb34079cf0b243fc3f3ac7edfae5a4b1572eb8ab254e0a2d5910ef0d0fae8134a49aa177cf8941e53874209d569cc7e05c1dbfaf68831666679a74e99428f8b60ac08709390ea2ac3f158effdd474490e43643265a098514ce55f66bde2fe1411dd84cd34fde55382aff9bc900da28caf0947dd',
     recaptcha: {
         siteKey: '6LdOqk0pAAAAAHJHTnJUH6ZdeXkZU19rVuTLF84a',
       },
+}; */
+export const environment = {
+  production: true,
+  apiUrl: 'https://admin.futureforceconsultants.com',
+  apiToken:'6e86f00dc37ec579e28c9846099c8d5895dbdc962321098319f0bf93fe6695213b96a8b1f9bd99c6e67583c137cc24d5a7308fe6e630db4d519b908d907467bc23cdd26487f774510c52f3d8e2c8823df60137f088276ccf5f74fd8bad878dae965590cc16ec2f1dbd468fc9fec55ded560b99f08237012e6a872d6add608d94',
+  recaptcha: {
+      siteKey: '6LdOqk0pAAAAAHJHTnJUH6ZdeXkZU19rVuTLF84a',
+    },
 };
